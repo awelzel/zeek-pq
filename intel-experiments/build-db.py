03cb8307777616ad1abefe7c12b9c90f2ef36209
Arne Welzel <arne.welzel@corelight.com>
2022-09-21 13:24:16 +0200

    intel-experiments: intel_ip table generation

---
+++ intel-experiments/build-db.py
@@ -0,0 +1,91 @@
+import argparse
+import ipaddress
+import re
+
+import postgresql
+import postgresql.versionstring
+
+# monkey patch postgresql.versionstring.split to accept 14.5 (Debian 14.5-1.pgdg110+1)
+# https://github.com/python-postgres/fe/issues/109
+
+_orig_split = postgresql.versionstring.split
+
+def _monkey_split(vstr):
+    m = re.match("^([0-9]+\.[0-9]+[^\s]*)\s+\(.+\)$", vstr)
+    if m is not None:
+        vstr = m.group(1)
+    return _orig_split(vstr)
+
+postgresql.versionstring.split = _monkey_split
+
+def rebuild_table(conn, count):
+    """
+    Build an intel_ip table with count IP address entries.
+    """
+    conn.execute("""
+        DROP TABLE IF EXISTS intel_ip;
+    """)
+
+    # This is a naive table layout. The source information may or may
+    # not be held in a separate table.
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS intel_ip (
+            ip inet,
+            source text,
+            description text,
+            url text);
+    """)
+
+    insert_stmt = conn.prepare("INSERT INTO intel_ip VALUES (($1::text)::inet, $2, $3, $4)");
+
+
+    # There's load_rows() which is probably much much faster.
+    start_ip = 100000000
+    for i in range(count):
+        i += 1
+
+        ip = ipaddress.ip_address(start_ip + i)
+        source = "source1-" + str(i)
+        description = "Generated IP - " + str(ip)
+        url = "http://localhost/intel/q=" + str(ip)
+
+        insert_stmt(str(ip), source, description, url)
+
+    # print("building gist index")
+    # conn.execute("""
+    #    CREATE INDEX idx_intel_ip_gist ON intel_ip USING gist (ip inet_ops);
+    #""")
+
+    # Very little testing indicates the hash index is faster. For 1mio
+    # entries it requires 32MB of space, while the gist takes up 30MB.
+    # We don't care about 2MB for 2mio entries.
+    print("building hash index")
+    conn.execute("""
+        CREATE INDEX idx_intel_ip_hash ON intel_ip USING hash(ip);
+    """)
+
+    print("vacuum analyze")
+    conn.execute("""
+        VACUUM ANALYZE intel_ip;
+    """)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--iri", default="pq://zeek@127.0.0.1/zeek")
+    parser.add_argument("--count", type=int, default=0)
+    parser.add_argument("--rebuild-table", action="store_true")
+    args = parser.parse_args()
+
+    conn = postgresql.open(args.iri)
+
+    if args.rebuild_table:
+        if args.count == 0:
+            raise parser.error("Use --count, please")
+        rebuild_table(conn, args.count)
+
+    else:
+        __import__('IPython').embed(banner1="")
+
+
+if __name__ == "__main__":
+    main()
